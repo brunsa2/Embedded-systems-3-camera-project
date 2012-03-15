4dcafb79899b4e8662418e0917d3d4c4d737876b
Jeff Stubler <brunsa2@gmail.com>
2012-03-14 23:33:33 -0500

    Complete test shell
    Complete test shell for testing serial with servo motors

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(void) {
 	char line[LINE_LENGTH], token[LINE_LENGTH];
-	uint32_t position;
+	int position;
 	usart_init(16000000, 9600, USART_TRANSMIT | USART_RECEIVE);
 	console_init(usart_putc, usart_getc);
 	pwm_init();
@@ -20,15 +20,35 @@ int main(void) {
 		fgets(line, LINE_LENGTH, stdin);
 		
 		if (sscanf(line, "%s", token)) {
-			if (strcmp(line, "horiz") == 0) {
-			} else if (strcmp(line, "vert") == 0) {
-			} else if (strcmp(line, "man") == 0) {
+			if (strcmp(token, "horiz") == 0 || strcmp(token, "vert") == 0) {
+				if (sscanf(line, "%s %d", token, &position) == 2) {
+
+					if (position < 0) position = 0;
+					if (position > 1000) position = 1000;
+					
+					if (strcmp(token, "horiz") == 0) {
+						printf("Moving to position %d horizontally\r\n", position);
+						pwm_set_pulse_length(PWM_CHANNEL_0, (uint16_t) (1000+position));
+					} else if (strcmp(token, "vert") == 0) {
+						printf("Moving to position %d vertically\r\n", position);
+						pwm_set_pulse_length(PWM_CHANNEL_1, (uint16_t) (1000+position));
+					}
+
+				} else {
+					printf("Invalid formed command \"%s\"\r\r", line);
+				}
+			} else if (strcmp(token, "man") == 0) {
+				printf("vert pos - Move vertically to position pos from 0 to 1000\r\n");
+				printf("horiz pos - Move horizontally to position pos from 0 to 1000\r\n");
+				printf("man - Show this page\r\n\r\n");
 			} else {
-				printf("Invalid command %s\r\n", token);
+				printf("Invalid command \"%s\"\r\n", token);
 			}
 		} else {
 			printf("Invalid entry %s\r\n", line);
 		}
+
+		strcpy(token, "");
 	}
 
 	return 0;
