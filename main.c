d5ff70e7d7343a296a564cc5a9daa3022541fbea
Jeff Stubler <brunsa2@gmail.com>
2012-03-14 23:03:31 -0500

    Add working test shell
    Add testing work shell for week 2 goals

--- main.c
+++ main.c
@@ -1,31 +1,23 @@
 #include "usart.h"
 #include "console.h"
-#include "os.h"
 #include "pwm.h"
 #include <stdio.h>
 
-void task_1(void);
-
-void task_1(void) {
-	while (1) {
-		printf("The quick brown fox jumps over a lazy dog. Please enter your name: \r\n");
-		char name[80];
-		fgets(name, 80, stdin);
-		printf("Hello, %s", name);
-
-		os_delay(1, 20000);
-	}
-}
+#define LINE_LENGTH 80
 
 int main(void) {
+	char line[LINE_LENGTH];
 	usart_init(16000000, 9600, USART_TRANSMIT | USART_RECEIVE);
 	console_init(usart_putc, usart_getc);
-	
 	pwm_init();
-	os_init();
-	os_add_task(task_1, "t1");
 	
-	while (1);
+	printf("\fEmbedded Systems III temporary shell\r\nfor demonstrations, week 2\r\n\r\n");
+
+	while (1) {
+		printf("> ");
+		fgets(line, LINE_LENGTH, stdin);
+		printf("%s\r\n", line);
+	}
 
 	return 0;
 }
