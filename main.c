800e82fb9f84ba8f2435041b7584a1c5687b40d3
Jeff Stubler <brunsa2@gmail.com>
2012-03-14 23:13:34 -0500

    Add command recognition
    Add start of command recognition

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include "console.h"
 #include "pwm.h"
 #include <stdio.h>
+#include <string.h>
 
 #define LINE_LENGTH 80
 
 int main(void) {
-	char line[LINE_LENGTH];
+	char line[LINE_LENGTH], token[LINE_LENGTH];
+	uint32_t position;
 	usart_init(16000000, 9600, USART_TRANSMIT | USART_RECEIVE);
 	console_init(usart_putc, usart_getc);
 	pwm_init();
@@ -16,7 +18,17 @@ int main(void) {
 	while (1) {
 		printf("> ");
 		fgets(line, LINE_LENGTH, stdin);
-		printf("%s\r\n", line);
+		
+		if (sscanf(line, "%s", token)) {
+			if (strcmp(line, "horiz") == 0) {
+			} else if (strcmp(line, "vert") == 0) {
+			} else if (strcmp(line, "man") == 0) {
+			} else {
+				printf("Invalid command %s\r\n", token);
+			}
+		} else {
+			printf("Invalid entry %s\r\n", line);
+		}
 	}
 
 	return 0;
