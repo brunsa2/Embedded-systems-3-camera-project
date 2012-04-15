b4c8909504d9d07827b8ffc543160f216a46094d
Jeff Stubler <brunsa2@gmail.com>
2012-04-14 21:04:46 -0500

    Modify sample code to meet specifications

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(void) {
 	char line[LINE_LENGTH], token[LINE_LENGTH];
-	int position;
+	int position, data;
 	usart_init(16000000, 9600, USART_TRANSMIT | USART_RECEIVE);
 	console_init(usart_putc, usart_getc);
 	pwm_init();
@@ -35,12 +35,27 @@ int main(void) {
 					}
 
 				} else {
-					printf("Invalid formed command \"%s\"\r\r", line);
+					printf("Invalid formed command \"%s\"\r\n", line);
 				}
-			} else if (strcmp(token, "man") == 0) {
+			} else if (strcmp(token, "rdio") == 0) {
+				if (sscanf(line, "%s %x", token, &position) == 2) {
+					printf("Reading address 0x%x is 0x%x\r\n", position, *(uint8_t *)position);
+				} else {
+					printf("Invalid formed command \"%s\"\r\n", line);
+				}	
+			} else if (strcmp(token, "wrio") == 0) {
+				if (sscanf(line, "%s %x %x", token, &position, &data) == 3) {
+					printf("Writing 0x%x to address 0x%x\r\n", position, data);
+					*(uint8_t *) position = (uint8_t) data;
+				} else {
+					printf("Invalid formed command \"%s\"\r\n", line);
+				}
+			} else if (strcmp(token, "help") == 0) {
 				printf("vert pos - Move vertically to position pos from 0 to 1000\r\n");
 				printf("horiz pos - Move horizontally to position pos from 0 to 1000\r\n");
-				printf("man - Show this page\r\n\r\n");
+				printf("rdio addr - Read address addr\r\n");
+				printf("wrio addr data - Write data data to address addr\r\n");
+				printf("help - Show this page\r\n\r\n");
 			} else {
 				printf("Invalid command \"%s\"\r\n", token);
 			}
